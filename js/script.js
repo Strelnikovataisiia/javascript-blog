953730936e8d303073a8b40075452e26fb97bd56
Taisiia Strelnikova <tayushka1236@gmail.com>
2022-11-20 21:51:13 +0100

    change active

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-
-  /* remove class 'active' from all article links  */
+    event.preventDefault();
+    const clickedElement=this;
+    console.log('Link was clicked!');
+  
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.add('active');
 
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
-
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
