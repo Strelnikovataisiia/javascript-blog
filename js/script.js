ff9aa4aaaf5d143a00b588c50b924e3bc438cfd7
Taisiia Strelnikova <tayushka1236@gmail.com>
2022-11-23 23:36:26 +0100

    Add active

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 function titleClickHandler(event){
     event.preventDefault();
-    const clickedElement=this;
+    const clickedElement = this;
     console.log('Link was clicked!');
   
   /* [DONE] remove class 'active' from all article links  */
@@ -11,20 +11,26 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
 }
   /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
-  clickedElement.add('active');
 
-  /* [DONE] remove class 'active' from all articles */
+  /* [?DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [in ] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('selector', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('target', targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  console.log('article active:', targetArticle);
 }
 
 const links = document.querySelectorAll('.titles a');
