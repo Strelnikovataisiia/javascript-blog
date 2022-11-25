c1c1e3d3e9ef2866a20f7745568fe69b05476d97
Taisiia Strelnikova <tayushka1236@gmail.com>
2022-11-25 16:59:28 +0100

    Add titlesJC

--- js/script.js
+++ js/script.js
@@ -33,8 +33,46 @@ function titleClickHandler(event){
   console.log('article active:', targetArticle);
 }
 
-const links = document.querySelectorAll('.titles a');
-
+/*const links = document.querySelectorAll('.titles a');
+  console.log('links', links);
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}*/
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  console.log('generate!');
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+    
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute('id')
+    console.log (articleId);
+    /* find the title element */
+
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      console.log (articleTitle);
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log (linkHTML);
+    /* insert link into titleList */
+    html = html + linkHTML;
+
+  }
+  console.log (html);
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+generateTitleLinks();
